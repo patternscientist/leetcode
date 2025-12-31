995abed75bb9fad1cb61224535a6565d6cc92f3d
patternscientist <noitcartsba@proton.me>
2025-12-30 21:14:51 -0500

    Time: 0 ms (100%), Space: 9.3 MB (11.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -6,17 +6,15 @@ public:
         stack<char> st;
         for (char c : s){
             // push any open paren onto the stack
-            if (c == '{' || c == '[' || c == '('){
+            if (c == '{' || c == '[' || c == '(')
                 st.push(c);
-            // if we see a closed paren instead, return false if...
-            } else if (st.empty()){ // there are no opens to match it with...
+            // if we see a closed paren instead, 
+            // return false if there are no opens to match it with
+            // or if the most recent open paren is the wrong kind...
+            else if (st.empty() || st.top() != mp[c]) 
                 return false;  
-            } else {
-                if (st.top() != mp[c]) // or if the most recent open paren is the wrong kind...
-                    return false; 
-                else 
-                    st.pop(); // otherwise we have a match so remove the last seen open paren
-            }
+            else 
+                st.pop(); // otherwise we have a match so remove the last seen open paren
         }
         return st.empty(); // by the end, s is valid iff there are no unmatched open parens
     }
