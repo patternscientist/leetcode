19b7b0046cfd9abffe7df07c7f70883065615faa
patternscientist <noitcartsba@proton.me>
2026-01-01 18:30:21 -0500

    Time: 7 ms (19.88%), Space: 9.8 MB (25.55%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        int n = s.size(), m = t.size();
-        if (n != m) return false;
+bool isAnagram(string s, string t) {
+    int n = s.size();
 
-        unordered_multiset<char> sSet = {}, tSet = {};
-        for (int i=0; i<n; i++){
-            sSet.insert(s[i]);
-            tSet.insert(t[i]);
-        }
-        return sSet == tSet;
+    if (t.size() != n) 
+        return false;
+
+    unordered_map<char,int> sMap, tMap;
+    char s_i, t_i;
+    
+    for (int i=0; i<n; ++i){
+        s_i = s[i]; t_i = t[i];
+        
+        if (auto it = sMap.find(s_i); it != sMap.end()) 
+            sMap[s_i]++;
+        else 
+            sMap[s_i] = 1;
+
+        if (auto it = tMap.find(t_i); it != tMap.end()) 
+            tMap[t_i]++;
+        else
+            tMap[t_i] = 1;
+
     }
+
+    return sMap == tMap;
+}
 };
