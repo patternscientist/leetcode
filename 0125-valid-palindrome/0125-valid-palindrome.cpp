3d93ec0008d2705b6a35f47153e4b7eadf0b9e20
patternscientist <noitcartsba@proton.me>
2026-01-01 17:34:53 -0500

    Update 0125-valid-palindrome.cpp
    Added a comment

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -8,7 +8,7 @@ public:
         // invariant: all (alphanumeric) characters strictly outside of [i, j] have been checked and matched
         while (i < j){
             // skip over any non-alphanumeric characters
-            while (i < j && !isalnum((unsigned char) s[i])) ++i;
+            while (i < j && !isalnum((unsigned char) s[i])) ++i; // char is signed on some platforms so cast to unsigned for safety
             while (i < j && !isalnum((unsigned char) s[j])) --j;
 
             if (tolower((unsigned char) s[i]) != tolower((unsigned char) s[j])) // be case-insensitive
