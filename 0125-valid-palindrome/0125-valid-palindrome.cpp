498fe1f716d97953778fac0166a382c49964d140
patternscientist <noitcartsba@proton.me>
2026-01-01 17:31:39 -0500

    Time: 0 ms (100%), Space: 9.9 MB (77.6%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -5,17 +5,16 @@ public:
         // two pointers
         int i = 0, j = n - 1;
 
-        // invariant: all (alphanumeric) characters strictly outside of [i, j] have been matched and checked
+        // invariant: all (alphanumeric) characters strictly outside of [i, j] have been checked and matched
         while (i < j){
             // skip over any non-alphanumeric characters
-            while (i < j && !isalnum(s[i])) ++i;
-            while (i < j && !isalnum(s[j])) --j;
+            while (i < j && !isalnum((unsigned char) s[i])) ++i;
+            while (i < j && !isalnum((unsigned char) s[j])) --j;
 
-            if (toupper(s[i]) != toupper(s[j])) // be case-insensitive
+            if (tolower((unsigned char) s[i]) != tolower((unsigned char) s[j])) // be case-insensitive
                 return false; 
             
-            ++i;
-            --j;           
+            ++i; --j;           
         }
 
         return true;
