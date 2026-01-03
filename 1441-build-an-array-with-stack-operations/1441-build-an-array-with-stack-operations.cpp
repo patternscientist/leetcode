ce0b563ab470a1a631640edb3f553ab74a80cfd7
patternscientist <noitcartsba@proton.me>
2026-01-03 03:47:31 -0500

    Time: 0 ms (100%), Space: 10.6 MB (84.55%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -3,7 +3,7 @@ public:
     vector<string> buildArray(vector<int>& target, int n) {
         vector<string> ans;
         int m = target.size(), i = 0, k = 1;
-        while (ans.size() != m){
+        while (i < m && k <= n){
             ans.push_back("Push");
             if (k != target[i]) ans.push_back("Pop");
             else i++;
