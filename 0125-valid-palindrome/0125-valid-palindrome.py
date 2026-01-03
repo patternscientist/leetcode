69ae2cbe532e03aba7a49b778571fd294fe0e6cf
patternscientist <noitcartsba@proton.me>
2026-01-02 23:46:27 -0500

    Time: 10 ms (40.64%), Space: 17.6 MB (95.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # string preprocessing:
-        s = s.upper() # turn all letters uppercase
-        s = ''.join(c for c in s if c.isalnum()) # and remove non-alphanumeric chars
-
         n = len(s)
-        # two pointers 
         i = 0
         j = n-1
 
-        # Invariant: all characters strictly outside [i, j] have been checked and matched; 
-        # i.e. s[0..i-1] mirrors s[j+1..n-1].
-        while (i < j):
-            if (s[i] != s[j]):
-                return False
+        while i < j:
+            while i < j and not s[i].isalnum(): i+=1
+            while i < j and not s[j].isalnum(): j-=1
+            if s[i].lower() != s[j].lower(): return False
             i += 1
             j -= 1
         
