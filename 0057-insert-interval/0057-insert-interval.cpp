59664538909a68593d9df98c23279dddb3ac1535
patternscientist <noitcartsba@proton.me>
2026-01-11 20:32:54 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,73 +1,23 @@
 class Solution {
 public:
-    vector<int> combineIntervals(vector<int>& i1, vector<int> i2){ 
-        int start_i1 = i1[0], end_i1 = i1[1], start_i2 = i2[0], end_i2 = i2[1];
-        if (start_i2 >= start_i1 && start_i2 <= end_i1 && end_i2 <= end_i1) // i2 is inside (or same as) i1; ({})
-            return {start_i1,end_i1};
-        else if (start_i1 >= start_i2 && start_i1 <= end_i2 && end_i1 <= end_i2) // i1 is inside (or same as) i2; {()}
-            return {start_i2,end_i2};
-        else if (start_i2 >= start_i1 && start_i2 <= end_i1 && end_i2 >= end_i1) // ({)}
-            return {start_i1,end_i2};
-        else if (start_i1 >= start_i2 && start_i1 <= end_i2 && end_i1 >= end_i2) // {(})
-            return {start_i2,end_i1};
-        else // no overlap
-            return {};
-        return {}; // shouldn't reach here
-    }
-    bool i1Belowi2(vector<int> i1, vector<int> i2){
-        return i1[1] < i2[0]; // end of i1 smaller than start of i2
-    }
-    bool noOverlap(vector<int> i1, vector<int> i2){
-        // no overlap == [start_new,end_new] comes strictly before or strictly after [start_i,end_i]:
-        return (i1Belowi2(i1,i2) || i1Belowi2(i2,i1));
-    }
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int n = intervals.size();
-        if (n == 0) return {newInterval};
-
-        int start_new = newInterval[0], end_new = newInterval[1], start_i, end_i;
-        if (start_new > intervals[n-1][1]){ // newInterval comes after all
-            intervals.push_back(newInterval);
-            return intervals;
-        } else if (end_new < intervals[0][0]){ // newInterval comes before all
-            vector<vector<int>> ans = {newInterval};
-            for (vector<int> interval : intervals)
-                ans.push_back(interval);
-            return ans;
-        } 
-
-        vector<int> intervals_i, candidate, mostRecent;
+        int n = intervals.size(), i = 0;
         vector<vector<int>> ans;
-        int ansSize;
-        for (int i=0; i<n; ++i){
-            ansSize = ans.size();
-            intervals_i = intervals[i];
-            start_i = intervals_i[0];
-            end_i   = intervals_i[1];
-
-            if (!ansSize == 0 &&
-                i1Belowi2(ans[ansSize-1],newInterval) && 
-                i1Belowi2(newInterval,intervals_i)){
-                candidate = newInterval;
-                i--; // cheeky move
-            } else if (i1Belowi2(intervals_i,newInterval))
-                candidate = intervals_i;
-            else if (i1Belowi2(newInterval,intervals_i))
-                candidate = intervals_i;
-            else 
-                candidate = combineIntervals(intervals_i,newInterval);
-            
-            if (ans.empty())
-                ans.push_back(candidate);
-            else{
-                mostRecent = ans.back();
-                if (noOverlap(mostRecent, candidate))
-                    ans.push_back(candidate);
-                else{
-                    ans.pop_back();
-                    ans.push_back(combineIntervals(mostRecent, candidate));
-                }
-            }
+        while (i < n && (intervals[i][1] < newInterval[0])){
+            ans.push_back(intervals[i]);
+            ++i;
+        }
+        while (i < n && 
+               ((intervals[i][1] >= newInterval[0] && intervals[i][1] <= newInterval[1]) || 
+                (intervals[i][0] >= newInterval[0] && intervals[i][0] <= newInterval[1]))){
+            newInterval[0] = min(intervals[i][0], newInterval[0]);
+            newInterval[1] = max(intervals[i][1], newInterval[1]);
+            ++i;
+        }
+        ans.push_back(newInterval);
+        while (i < n){
+            ans.push_back(intervals[i]);
+            ++i;
         }
         return ans;
     }
