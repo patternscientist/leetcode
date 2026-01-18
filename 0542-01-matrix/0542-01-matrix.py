627d049a37565685a153c4faa9c3632c1f17267b
patternscientist <noitcartsba@proton.me>
2026-01-18 15:24:56 -0500

    Time: 107 ms (85.99%), Space: 22.3 MB (14.89%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,28 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+        dist = [[-1 for _ in range(n)] for _ in range(m)]
+        q = deque(maxlen=m*n)
+
+        for r in range(m):
+            for c in range(n):
+                if mat[r][c] == 0:
+                    dist[r][c] = 0
+                    q.append((r,c))
+        
+        dr = [0,-1,0,1]
+        dc = [-1,0,1,0]
+
+        while q:
+            (r,c) = q.popleft()
+            for k in range(4):
+                nr = r + dr[k]
+                nc = c + dc[k]
+                if (0 <= nr and nr < m and
+                    0 <= nc and nc < n and 
+                    dist[nr][nc] == -1):
+                    dist[nr][nc] = dist[r][c] + 1
+                    q.append((nr,nc))
+        
+        return dist
