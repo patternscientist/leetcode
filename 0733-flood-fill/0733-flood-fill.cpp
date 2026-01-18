1426beae50b32765ccaf1c61aee45c4364bcf986
patternscientist <noitcartsba@proton.me>
2026-01-18 15:16:43 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int ogColor = image[sr][sc], n = image[0].size(), m = image.size(), i, j;
-        if (ogColor == color) return image;
+        int og = image[sr][sc];
+        if (og == color) return image;
+        
+        int m = image.size(), n = image.size();
+        queue<pair<int,int>> q;
+        image[sr][sc] = color;
+        q.push({sr,sc});
 
-        stack<pair<int,int>> st;
-        st.push({sr,sc});
+        int dr[4] = {0,-1,0,1};
+        int dc[4] = {-1,0,1,0};
+        int nr, nc;
 
-        pair<int,int> curr;
-        bool hasUnvisitedNb;
-
-        while (!st.empty()){
-            hasUnvisitedNb = false;        
-                
-            curr = st.top();
-            i = curr.first; j = curr.second;
-            image[i][j] = color;
-
-            vector<pair<int,int>> nbs {{i-1,j},   // down,right,up,left
-                                    {i+1,j},
-                                    {i,j+1},
-                                    {i,j-1}};
-
-            for (pair<int,int> nb : nbs){
-                i = nb.first; j = nb.second;
-                if (i >= 0 && i < m && 
-                    j >= 0 && j < n && 
-                    image[i][j] == ogColor){
-                    st.push(nb);
-                    hasUnvisitedNb = true;
-                    break;
-                }
+        while (!q.empty()){
+            auto [r,c] = q.front(); q.pop();
+            for (int k=0; k<4; ++k){
+                nr = r + dr[k];
+                nc = c + dc[k];
+                if (0 <= nr && nr < m &&
+                    0 <= nc && nc < n &&
+                    image[nr][nc] == og){
+                        image[nr][nc] = color;
+                        q.push({nr,nc});
+                    }
             }
-            
-            if (!hasUnvisitedNb) st.pop();
         }
 
         return image;
