942da874dc878044b276539a07257307c1d6df2c
patternscientist <noitcartsba@proton.me>
2026-01-19 12:28:10 -0500

    Time: 26 ms (86.64%), Space: 61.6 MB (98.55%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        sort(points.begin(), points.end(), [](auto v, auto w){
-                                                return pow((pow(v[0],2) + pow(v[1],2)),0.5)
-                                                < pow((pow(w[0],2) + pow(w[1],2)),0.5); });
-        vector<vector<int>> kClosestPoints(k);
-        for (int i=0; i<k; ++i)
-            kClosestPoints[i] = points[i];
-        return kClosestPoints;
+        auto d = [](const vector<int>& p){
+            return 1LL*p[0]*p[0] + 1LL*p[1]*p[1];
+        };
+        nth_element(points.begin(), points.begin()+k, points.end(),
+                    [&d](const vector<int>& p, const vector<int>& q){
+                        return d(p) < d(q);
+                    });
+        points.resize(k);
+        return points;
     }
 };
