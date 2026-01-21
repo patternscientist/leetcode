d3c9d51b9cf49b8493741e2f5d0d12896e36c2fc
patternscientist <noitcartsba@proton.me>
2026-01-21 17:13:54 -0500

    Time: 4 ms (46.32%), Space: 12.6 MB (64.97%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,10 +1,14 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        mp = {')':'(', ']':'[', '}':'{'}
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        mp = {')':'(', '}':'{',']':'['}
         st = deque()
         for c in s:
-            if c == '(' or c == '[' or c == '{':
+            if c == '(' or c == '{' or c == '[':
                 st.append(c)
-            elif len(st) == 0 or mp[c] != st.pop():
+            elif len(st) == 0 or st.pop() != mp[c]:
                 return False
         return len(st) == 0
