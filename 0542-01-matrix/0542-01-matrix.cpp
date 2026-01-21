d3d6a8662e64892c8e19adf8d02467794866af62
patternscientist <noitcartsba@proton.me>
2026-01-21 17:04:45 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -14,21 +14,24 @@ public:
             }
         }
 
-        int dr[4] = {1,-1,0,0}, dc[4] = {0,0,1,-1};
-        int nr, nc;
+        int dr[4] = {0,-1,0,1};
+        int dc[4] = {-1,0,1,0};
+        int nr,nc;
+
         while (!q.empty()){
             auto [r,c] = q.front(); q.pop();
             for (int k=0; k<4; ++k){
                 nr = r + dr[k];
                 nc = c + dc[k];
-                if (0 <= nr && nr < m && 
-                    0 <= nc && nc < n &&
+                if (0 <= nr && nr < m &&
+                    0 <= nc && nc < m &&
                     dist[nr][nc] == -1){
                         dist[nr][nc] = dist[r][c] + 1;
                         q.push({nr,nc});
                     }
             }
         }
+
         return dist;
     }
 };
