53e7e746aa33a28d8c138584b9dc392782df5d5f
patternscientist <noitcartsba@proton.me>
2026-01-23 16:27:49 -0500

    Time: 20 ms (93.45%), Space: 61.8 MB (91.93%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -4,7 +4,7 @@ public:
         auto d = [](const vector<int>& p){
             return 1LL*p[0]*p[0] + 1LL*p[1]*p[1];
         };
-        nth_element(points.begin(), points.begin()+k, points.end(),
+        nth_element(points.begin(), points.begin()+k, points.end(), 
                     [&d](const vector<int>& p, const vector<int>& q){
                         return d(p) < d(q);
                     });
