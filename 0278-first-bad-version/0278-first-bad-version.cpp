d0b8768ca86e4e086b139f4d560d04039b4eb874
patternscientist <noitcartsba@proton.me>
2026-01-22 20:20:52 -0500

    Time: 0 ms (100%), Space: 7.7 MB (93.77%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,16 +4,14 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        long long l = 1, r = 1LL*n, mid;
-        while (l<=r){
-            mid = (l+r)/2;
-            if (!isBadVersion(mid)) 
-                l=mid+1;
-            else if (mid>=1 && isBadVersion(mid-1))
-                r=mid-1;
-            else
-                return mid;
+        int l = 1, r = n, mid;
+        while (l < r){
+            mid = l + (r - l) / 2;
+            if (isBadVersion(mid))
+                r = mid;
+            else 
+                l = mid + 1;
         }
-        return -1;
+        return r;
     }
 };
