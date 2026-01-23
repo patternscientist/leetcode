a6cf602f5450232916409a66d0eb862c58be305f
patternscientist <noitcartsba@proton.me>
2026-01-23 16:42:51 -0500

    Time: 1 ms (46.56%), Space: 9.9 MB (54.32%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int n = s.size();
-        // two pointers
-        int i = 0, j = n - 1;
-
-        // invariant: all (alphanumeric) characters strictly outside of [i, j] have been checked and matched
+        int n = s.size(), i = 0, j = n-1;
         while (i < j){
-            // skip over any non-alphanumeric characters
-            while (i < j && !isalnum((unsigned char) s[i])) ++i; // char is signed on some platforms so cast to unsigned for safety
-            while (i < j && !isalnum((unsigned char) s[j])) --j;
-
-            if (tolower((unsigned char) s[i]) != tolower((unsigned char) s[j])) // be case-insensitive
-                return false; 
-            
-            ++i; --j;           
+            while (i < j && !isalnum(s[i])) ++i;
+            while (i < j && !isalnum(s[j])) --j;
+            if (tolower(s[i]) != tolower(s[j]))
+                return false;
+            ++i;
+            --j;
         }
-
         return true;
     }
 };
