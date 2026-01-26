c5081567449dca6b89c8d8fd9d28211b942e9597
patternscientist <noitcartsba@proton.me>
2026-01-26 15:48:28 -0500

    Time: 0 ms (100%), Space: 12.5 MB (26.57%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,26 +1,40 @@
-class MyQueue:
+class MyQueue(object):
 
     def __init__(self):
-        self.in_  = deque()
+        self.in_ = deque()
         self.out = deque()
 
-    def pour(self) -> None:
-        if len(self.out) != 0: return
+    def pour(self):
+        if (len(self.out) != 0): return 
         while len(self.in_) != 0:
             self.out.append(self.in_.pop())
 
-    def push(self, x: int) -> None:
-        self.in_.append(x)
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.in_.append(x)        
+        
 
-    def pop(self) -> int:
+    def pop(self):
+        """
+        :rtype: int
+        """
         self.pour()
         return self.out.pop()
 
-    def peek(self) -> int:
+    def peek(self):
+        """
+        :rtype: int
+        """
         self.pour()
         return self.out[-1]
 
-    def empty(self) -> bool:
+    def empty(self):
+        """
+        :rtype: bool
+        """
         return len(self.in_) == 0 and len(self.out) == 0
 
 
