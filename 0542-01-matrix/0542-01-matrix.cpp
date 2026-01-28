755383c1cce587c1ae2440c31384b780e2e520ca
patternscientist <noitcartsba@proton.me>
2026-01-27 21:40:28 -0500

    Time: 3 ms (98.57%), Space: 34.6 MB (84.4%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,7 +4,6 @@ public:
         int m = mat.size(), n = mat[0].size();
         vector<vector<int>> dist(m, vector<int>(n, -1));
         queue<pair<int,int>> q;
-
         for (int r=0; r<m; ++r){
             for (int c=0; c<n; ++c){
                 if (mat[r][c] == 0){
@@ -13,25 +12,22 @@ public:
                 }
             }
         }
-
         int dr[4] = {0,-1,0,1};
         int dc[4] = {-1,0,1,0};
         int nr,nc;
-
         while (!q.empty()){
             auto [r,c] = q.front(); q.pop();
             for (int k=0; k<4; ++k){
                 nr = r + dr[k];
                 nc = c + dc[k];
                 if (0 <= nr && nr < m &&
-                    0 <= nc && nc < m &&
+                    0 <= nc && nc < n && 
                     dist[nr][nc] == -1){
                         dist[nr][nc] = dist[r][c] + 1;
                         q.push({nr,nc});
                     }
             }
         }
-
         return dist;
     }
 };
