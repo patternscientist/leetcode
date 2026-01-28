afebb583f48cf1475563e0a7b9e490b181ed6997
patternscientist <noitcartsba@proton.me>
2026-01-28 17:08:16 -0500

    Time: 0 ms (100%), Space: 12.6 MB (24.82%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -12,10 +12,10 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if (!root) return nullptr;
+        if (!root) return root;
         swap(root->left, root->right);
         invertTree(root->left);
         invertTree(root->right);
         return root;
-    }
+    }   
 };
