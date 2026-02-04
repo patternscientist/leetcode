b45b01d56c6a393d5fa95bc786b566c89e9bdb7d
patternscientist <noitcartsba@proton.me>
2026-02-03 16:12:57 -0800

    Time: 6 ms (91.73%), Space: 11.8 MB (80.49%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,8 +9,8 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode *slow = head;
-        ListNode *fast = head;
+        ListNode* slow = head;
+        ListNode* fast = head;
         while (fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
