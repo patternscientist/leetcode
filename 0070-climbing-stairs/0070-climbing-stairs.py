dfc017cea2f23abb94cbe5026300805a8edfb316
patternscientist <noitcartsba@proton.me>
2026-02-05 00:13:50 -0800

    Time: 0 ms (100%), Space: 12.4 MB (87.52%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = deque(maxlen=2)
+        dp.append(1)
+        dp.append(1)
+        for _ in range(2,n+1):
+            dp.append(dp[0]+dp[1])
+        return dp[-1]
+        
