c04f065878542eaf26cc920be65467e0e543959c
patternscientist <noitcartsba@proton.me>
2026-02-06 00:06:38 -0800

    Time: 0 ms (100%), Space: 11.8 MB (72.92%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         int n = ransomNote.size(), m = magazine.size();
-        if (m < n) return false;
-        int cnt[26] = {0};
-        for (int i=0; i<m; ++i)
-            ++cnt[magazine[i] - 'a'];
-        for (int j=0; j<n; ++j)
-            if (--cnt[ransomNote[j] - 'a'] < 0) return false;
+        if (n > m) return false;
+        int seen[26];
+        for (char c : magazine)
+            ++seen[c-'a'];
+        for (char c : ransomNote)
+            if (--seen[c-'a'] < 0) return false;
         return true;
+        
     }
 };
