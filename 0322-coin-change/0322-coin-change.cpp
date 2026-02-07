d91415f513b10f0a69b824b8b45ceedaedfe96e9
patternscientist <noitcartsba@proton.me>
2026-02-07 00:33:43 -0800

    Time: 18 ms (82.89%), Space: 17.7 MB (92.69%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        const int INF = amount + 1;
-        vector<int> dp(amount+1, INF);
+        int INF = amount + 1;
+        vector<int> dp(amount+1,INF);
         dp[0] = 0;
-        for (int x=1; x<=amount; ++x){
-            for (int c : coins)
-                if (c <= x) dp[x] = min(dp[x],dp[x-c]+1);
+        for (int n=1; n<=amount; ++n){
+            for (int denom : coins){
+                if (denom <= n) 
+                    dp[n] = min(dp[n],dp[n-denom]+1);
+            }
         }
         return dp[amount] == INF ? -1 : dp[amount];
     }
