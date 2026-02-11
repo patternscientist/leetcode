f26f50b85a2d586afbef812e16fa488d380ddd2e
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-02-11 04:15:57 -0500

    Time: 0 ms (100%), Space: 21.1 MB (29.67%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,23 +1,19 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def ok(self,root,low,high):
-        if root == None: return True
-        if not ((low < root.val) and (root.val < high)):
+class Solution:
+    def ok(self, root: Optional[TreeNode], low: int, high: int) -> bool:
+        if not root: return True
+        if root.val <= low or root.val >= high:
             return False
-        return (self.ok(root.left,low,root.val) and 
-                self.ok(root.right,root.val,high))
+        if not root.left and not root.right:
+            return True
+        return self.ok(root.left,low,root.val) and self.ok(root.right,root.val,high)
 
-    def isValidBST(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: bool
-        """
-        EXCLUSIVE_MIN = -2**31-1
-        EXCLUSIVE_MAX = 2**31
-        return self.ok(root,EXCLUSIVE_MIN,EXCLUSIVE_MAX)
-        
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        EXCLUSIVE_LOW  = -2**31-1
+        EXCLUSIVE_HIGH = 2**31
+        return self.ok(root,EXCLUSIVE_LOW,EXCLUSIVE_HIGH)
