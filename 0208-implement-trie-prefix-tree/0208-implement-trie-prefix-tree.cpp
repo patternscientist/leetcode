90d110108f3cbaac0da70603c500f91e1b4cdba4
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-02-11 03:53:41 -0500

    Time: 25 ms (39.64%), Space: 50.6 MB (53.93%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,12 +1,12 @@
 class Trie {
     struct Node{
         bool end = false;
-        array<Node*,26> next{};
+        array<Node*,26> nxt{};
         Node() = default;
     };
 
 public:
-    Node* root;
+    Node* root = nullptr;
 
     Trie() {
         root = new Node();
@@ -14,30 +14,33 @@ public:
     
     void insert(string word) {
         Node* cur = root;
-        for (char c : word){
-            int i = c-'a';
-            if (!cur->next[i]) cur->next[i] = new Node();
-            cur = cur->next[i];
+        for (char ch : word){
+            int i = ch-'a';
+            if (!cur->nxt[i])
+                cur->nxt[i] = new Node();
+            cur = cur->nxt[i];
         }
         cur->end = true;
     }
     
     bool search(string word) {
         Node* cur = root;
-        for (char c : word){
-            int i = c-'a';
-            if (!cur->next[i]) return false;
-            cur = cur->next[i];
+        for (char ch : word){
+            int i = ch-'a';
+            if (!cur->nxt[i])
+                return false;
+            cur = cur->nxt[i];
         }
         return cur->end;
     }
     
     bool startsWith(string prefix) {
         Node* cur = root;
-        for (char c : prefix){
-            int i = c-'a';
-            if (!cur->next[i]) return false;
-            cur = cur->next[i];
+        for (char ch : prefix){
+            int i = ch-'a';
+            if (!cur->nxt[i])
+                return false;
+            cur = cur->nxt[i];
         }
         return true;
     }
