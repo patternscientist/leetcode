bff63eb705d5e71e4c164424308041e1d2621ee6
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-02-14 16:13:19 -0500

    Time: 0 ms (100%), Space: 7.9 MB (70.74%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,6 +5,7 @@ public:
         int b = 1; // ways to get 1
         for (int k=2; k<=n; ++k){
             int c = a + b; // ways to get k
+            // shift window
             a = b;
             b = c;
         }
