d713096ad9d135185429f59f33ec939df7e5f4b6
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-02-15 18:20:13 -0500

    Time: 0 ms (100%), Space: 16.7 MB (87.14%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,41 +3,40 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+        int fresh = 0;
         int dr[4] = {0,-1,0,1};
         int dc[4] = {-1,0,1,0};
         queue<pair<int,int>> q;
-        vector<pair<int,int>> fresh;
         for (int r=0; r<m; ++r){
             for (int c=0; c<n; ++c){
                 if (grid[r][c] == 2)
                     q.push({r,c});
                 else if (grid[r][c] == 1)
-                    fresh.push_back({r,c});
+                    ++fresh;
             }
         }
-        int minMinutes = 0;
+        int minutes = 0;
         while (!q.empty()){
             int sz = q.size();
             bool madeRotten = false;
-            for (int k=0; k<sz; ++k){
+            for (int i=0; i<sz; ++i){
                 auto [r,c] = q.front(); q.pop();
-                for (int l=0; l<4; ++l){
-                    int nr = r + dr[l];
-                    int nc = c + dc[l];
+                for (int k=0; k<4; ++k){
+                    int nr = r + dr[k];
+                    int nc = c + dc[k];
                     if (0 <= nr && nr < m &&
-                        0 <= nc && nc < n &&
+                        0 <= nc && nc < n && 
                         grid[nr][nc] == 1){
                             grid[nr][nc] = 2;
                             q.push({nr,nc});
                             madeRotten = true;
+                            --fresh;
                         }
                 }
             }
             if (madeRotten)
-                ++minMinutes;
+                ++minutes;
         }
-        for (const pair<int,int>& pos : fresh)
-            if (grid[pos.first][pos.second] == 1) return -1;
-        return minMinutes;
+        return fresh == 0 ? minutes : -1;
     }
 };
