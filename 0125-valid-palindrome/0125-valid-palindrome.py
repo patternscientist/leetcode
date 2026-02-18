799ed05efd989a521b58296e696534621c5b8487
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-02-18 03:08:10 -0500

    Time: 11 ms (35.12%), Space: 19.6 MB (74.06%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,17 +1,12 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        n = len(s)
-        i = 0
-        j = n-1
-        while i<j:
-            while i<j and not s[i].isalnum(): i+=1
-            while i<j and not s[j].isalnum(): j-=1
-            if s[i].lower() != s[j].lower(): return False # be case-insensitive
-            i+=1
-            j-=1
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum(): l += 1
+            while l < r and not s[r].isalnum(): r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
         return True
-        
