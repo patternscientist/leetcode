a8ec488766296ba773934c8b49dca13b9ff567dd
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-02-18 18:15:02 -0500

    Time: 3 ms (77.68%), Space: 19.4 MB (59.86%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,8 +2,8 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        q = deque()
         fresh = 0
+        q = deque()
         for r in range(m):
             for c in range(n):
                 if grid[r][c] == 2:
@@ -22,12 +22,12 @@ class Solution:
                     nr = r + dr[k]
                     nc = c + dc[k]
                     if (0 <= nr and nr < m and
-                        0 <= nc and nc < n and
+                        0 <= nc and nc < n and 
                         grid[nr][nc] == 1):
                         grid[nr][nc] = 2
-                        q.append((nr,nc))
-                        madeRotten = True
                         fresh -= 1
-            if madeRotten: 
+                        madeRotten = True
+                        q.append((nr,nc))
+            if madeRotten:
                 minutes += 1
         return minutes if fresh == 0 else -1
