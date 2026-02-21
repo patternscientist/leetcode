3253a69312d57e740733cad8e0048912ce3dfd4c
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-02-21 15:49:42 -0500

    Time: 4 ms (38.96%), Space: 14.2 MB (45.46%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> ans;
     vector<int> path;
-    void dfs(int start, int remaining, const vector<int>& cand){
+    void dfs(int start, int remaining, vector<int>& cand){
         if (remaining == 0){
             ans.push_back(path);
             return;
@@ -11,7 +11,6 @@ public:
             return;
         for (int i=start; i<cand.size(); ++i){
             int x = cand[i];
-            if (x > remaining) break;
             path.push_back(x);
             dfs(i,remaining-x,cand);
             path.pop_back();
@@ -19,7 +18,6 @@ public:
         return;
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
         dfs(0,target,candidates);
         return ans;
     }
