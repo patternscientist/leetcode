25e37e45cf3ed9ba81d7ad55d7442607c4c32e13
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-02-21 15:39:00 -0500

    Time: 0 ms (100%), Space: 9.2 MB (10.99%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     bool isValid(string s) {
-        // initialize a map and declare a stack
-        unordered_map<char,char> mp = {{'}','{'},{']','['},{')','('}}; // closed -> open
+        unordered_map<char,char> mp = {{')','('},{'}','{'},{']','['}};
         stack<char> st;
-        for (char c : s){
-            // push any open paren onto the stack
-            if (c == '{' || c == '[' || c == '(')
-                st.push(c);
-            // if we see a closed paren instead, 
-            // return false if there are no opens to match it with
-            // or if the most recent open paren is the wrong kind...
-            else if (st.empty() || st.top() != mp[c]) 
-                return false;  
-            else 
-                st.pop(); // otherwise we have a match so pop the last seen open paren
+        for (char ch : s){
+            if (ch == '(' || 
+                ch == '{' || 
+                ch == '[')
+                st.push(ch);
+            else if (st.empty() || mp[ch] != st.top())
+                return false;
+            else
+                st.pop();
         }
-        return st.empty(); // by the end, s is valid iff there are no unmatched open parens
+        return st.empty();
     }
 };
