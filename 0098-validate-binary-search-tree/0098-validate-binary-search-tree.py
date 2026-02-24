5259346a0d7f21b247021979e2205d3beb379390
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-02-23 23:36:56 -0500

    Time: 0 ms (100%), Space: 21 MB (38.24%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -5,15 +5,12 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def ok(self, root: Optional[TreeNode], low: int, high: int) -> bool:
-        if not root: return True
-        if root.val <= low or root.val >= high:
-            return False
-        if not root.left and not root.right:
-            return True
-        return self.ok(root.left,low,root.val) and self.ok(root.right,root.val,high)
-
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        EXCLUSIVE_LOW  = -2**31-1
-        EXCLUSIVE_HIGH = 2**31
-        return self.ok(root,EXCLUSIVE_LOW,EXCLUSIVE_HIGH)
+        def dfs(root : Optional[TreeNode], exclusiveLow : int, exclusiveHigh : int) -> bool:
+            if not root: return True 
+            if root.val <= exclusiveLow or root.val >= exclusiveHigh:
+                return False
+            return dfs(root.left,exclusiveLow,root.val) and dfs(root.right,root.val,exclusiveHigh)
+        exclusiveLow  = -2**31-1
+        exclusiveHigh = 2**31
+        return dfs(root,exclusiveLow,exclusiveHigh)
