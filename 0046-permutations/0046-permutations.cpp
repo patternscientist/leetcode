662c0da062563ed5c69de8f4dac063a5468d8108
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-02-28 04:36:45 -0500

    Time: 0 ms (100%), Space: 10.8 MB (35.99%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     vector<vector<int>> ans;
     vector<int> path;
-    void dfs(const vector<int>& nums, const int numsSize, vector<bool>& used) {
-        if (path.size() == numsSize){
+    void dfs(const vector<int>& nums, const int n, vector<bool>& used){
+        if (path.size() == n){
             ans.push_back(path);
             return;
         }
-        for (int i=0; i<numsSize; ++i){
-            if (used[i])
-                continue;
+        for (int i=0; i<n; ++i){
+            if (used[i]) continue;
             used[i] = true;
             int x = nums[i];
             path.push_back(x);
-            dfs(nums,numsSize,used);
+            dfs(nums,n,used);
             path.pop_back();
             used[i] = false;
         }
