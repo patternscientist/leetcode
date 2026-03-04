2500c366da18e3ba6ec7817d354437d982f44d21
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-03-04 03:10:27 -0500

    Time: 3 ms (77.03%), Space: 19 MB (93.69%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -3,20 +3,24 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
         vector<int> indeg(numCourses);
-        for (const auto& p : prerequisites){
-            int a = p[0], b = p[1];
-            adj[b].push_back(a);
-            indeg[a]++;
+        for (const vector<int>& preq : prerequisites){
+            int a_i = preq[0], b_i = preq[1];
+            adj[b_i].push_back(a_i);
+            indeg[a_i]++;
         }
         queue<int> q;
-        for (int i=0; i<numCourses; ++i)
-            if (indeg[i] == 0) q.push(i);
+        for (int c=0; c<numCourses; c++){
+            if (indeg[c] == 0)
+                q.push(c);
+        }
         int taken = 0;
         while (!q.empty()){
-            int v = q.front(); q.pop();
-            for (const auto& u : adj[v])
-                if (--indeg[u] == 0) q.push(u);
+            int cur = q.front(); q.pop();
             taken++;
+            for (int c : adj[cur]){
+                if (--indeg[c] == 0)
+                    q.push(c);
+            }
         }
         return taken == numCourses;
     }
