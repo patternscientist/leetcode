71117eb4baa6cd26f3674670fa9fb82cbe3ac043
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-03-06 01:36:08 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = (int)nums.size();
-        unordered_map<int,int> mp;
-        for (int num : nums){
-            if (!mp.count(num))
-                mp[num] = 0;
-            mp[num]++;
-            if (mp[num] > n/2)
-                return num;
+        int candidate = 0;
+        int count = 0;
+        for (int x : nums){
+            if (count == 0){
+                candidate = x;
+                count++;
+            } else if (candidate == x)
+                count++;
+            else
+                count--;
         }
-        return (int)pow(10,9)+1; // shouldn't be able to reach here
+        return candidate;
     }
 };
