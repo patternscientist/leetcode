22b07b193f925597b78a11837fb7c19e03baa46d
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-03-05 23:33:07 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,11 +4,10 @@ public:
         int INF = amount+1;
         vector<int> dp(amount+1,INF);
         dp[0] = 0;
-        for (int n=1; n<amount+1; ++n){
-            for (int denom : coins){
+        for (int n=1; n<=amount; ++n){
+            for (int denom : coins)
                 if (n - denom >= 0)
                     dp[n] = min(dp[n],dp[n-denom]+1);
-            }
         }
         return dp[amount] == INF ? -1 : dp[amount];
     }
