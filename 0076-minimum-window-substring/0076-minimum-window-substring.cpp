53cd680605dfea39175e572d7c3e2daaf332f6b1
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-03-10 02:49:32 -0400

    Time: 1 ms (84.02%), Space: 11.2 MB (93.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int m = (int)s.size(), n = (int)t.size();
+        int m = s.size(), n = t.size();
         if (m < n) return "";
         vector<int> need(128,0);
-        for (char ch : t) need[ch]++;
+        for (const char& ch : t)
+            need[ch]++;
         int missing = n;
-        int l = 0;
-        int bestLen = INT_MAX;
         int bestStart = 0;
+        int bestLen = INT_MAX;
+        int l = 0;
         for (int r=0; r<m; ++r){
             char cr = s[r];
             if (need[cr] > 0)
@@ -17,7 +18,7 @@ public:
             while (missing == 0){
                 int winLen = r-l+1;
                 if (winLen < bestLen){
-                    bestLen = winLen;
+                    bestLen   = winLen;
                     bestStart = l;
                 }
                 char cl = s[l];
