1decfe0b0503ba6e18fef1b6b92569734ad04249
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-03-10 02:24:57 -0400

    Time: 11 ms (99.51%), Space: 36.3 MB (33.26%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -13,16 +13,16 @@ public:
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
         if (!root) return "";
+        string s;
         queue<TreeNode*> q;
         q.push(root);
-        string s;
         while (!q.empty()){
             TreeNode* cur = q.front(); q.pop();
             if (cur){
-                s += (to_string(cur->val) + "#");
+                s += (to_string(cur->val) + '#');
                 q.push(cur->left);
                 q.push(cur->right);
-            } else
+            } else 
                 s += "n#";
         }
         return s;
@@ -33,27 +33,26 @@ public:
         if (data.empty()) return nullptr;
         vector<string> vals;
         string token;
-        for (char ch : data){
+        for (const char& ch : data){
             if (ch == '#'){
                 vals.push_back(token);
                 token.clear();
-            } else 
+            } else
                 token.push_back(ch);
         }
-        if (vals[0] == "n") return nullptr;
         TreeNode* root = new TreeNode(stoi(vals[0]));
         queue<TreeNode*> q;
         q.push(root);
         int i = 1;
-        while (!q.empty() && i < (int)vals.size()){
+        int n = vals.size();
+        while (!q.empty() && i < n){
             TreeNode* cur = q.front(); q.pop();
-            if (i < (int)vals.size() && vals[i] != "n"){
+            if (i < n && vals[i] != "n"){
                 cur->left = new TreeNode(stoi(vals[i]));
                 q.push(cur->left);
             }
-            
             i++;
-            if (i < (int)vals.size() && vals[i] != "n"){
+            if (i < n && vals[i] != "n"){
                 cur->right = new TreeNode(stoi(vals[i]));
                 q.push(cur->right);
             }
