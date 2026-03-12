72c4e9fc0657601388e1f2c6da1b7a7b7e92fe54
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-03-12 03:11:04 -0400

    Time: 0 ms (100%), Space: 19.4 MB (49.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,9 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        s_prev = 1 # ways to get 0
-        s_now  = 1 # ways to get 1
+        a = 1
+        b = 1
         for k in range(2,n+1):
-            s_next = s_prev + s_now # ways to get k
-            s_prev = s_now
-            s_now = s_next
-        return s_now
+            c = a + b
+            a = b
+            b = c
+        return b
