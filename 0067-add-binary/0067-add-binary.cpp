6e37282be09c8026ad681cf41e0fce8eb04f8f5a
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-03-18 18:53:47 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -11,19 +11,15 @@ public:
             a = padding + a;
         int carry  = 0;
         int curSum = 0;
-        stack<char> ansStack;
         string ans;
         for (int i=max(m,n)-1; i>=0; --i){
             curSum = (a[i]-'0') + (b[i]-'0');
-            ansStack.push('0' + ((curSum + carry) % 2));
+            ans.push_back('0' + ((curSum + carry) % 2));
             carry  = curSum + carry > 1 ? 1 : 0;
             if (i == 0 && carry == 1)
-                ansStack.push('1');
+                ans.push_back('1');
         }
-        while (!ansStack.empty()){
-            ans.push_back(ansStack.top()); 
-            ansStack.pop();
-        }
+        reverse(ans.begin(),ans.end());
         return ans;
 
     }
