f9da2b678c62f01072f680af42d4c16ae53f006c
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-03-18 19:46:42 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,17 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        i = len(a)-1
+        j = len(b)-1
+        carry = 0
+        ans = ""
+        while i >= 0 or j >= 0 or carry:
+            sum_ = carry
+            if i >= 0:
+                sum_ += int(a[i])
+                i -= 1
+            if j >= 0:
+                sum_ += int(b[j])
+                j -= 1
+            ans += str(sum_ & 1)
+            carry = sum_ >> 1
+        return ''.join(list(reversed(ans)))
