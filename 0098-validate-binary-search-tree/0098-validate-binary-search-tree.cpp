02a52917cc315c73a2a5e35f7ac2d0db1238c912
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-03-24 16:46:39 -0400

    Time: 0 ms (100%), Space: 21.8 MB (94.54%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,12 +11,14 @@
  */
 class Solution {
 public:
-    bool ok(TreeNode* root, long long low, long long high){
-        if (!root) return true;
-        if (root->val <= low || root->val >= high)
+    bool ok(TreeNode* root, long long minVal, long long maxVal){
+        if (!root)
+            return true;
+        if (root->val <= minVal || root->val >= maxVal)
             return false;
-        return ok(root->left,low,root->val) && ok(root->right,root->val,high);
+        return ok(root->left,minVal,root->val) && ok(root->right,root->val,maxVal);
     }
+
     bool isValidBST(TreeNode* root) {
         return ok(root,LLONG_MIN,LLONG_MAX);
     }
