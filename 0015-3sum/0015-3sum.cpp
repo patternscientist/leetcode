0a5bffd5d270e237352104d926c131ed737041f6
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-03-24 17:16:55 -0400

    Time: 39 ms (95.14%), Space: 29.1 MB (45.6%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(),nums.end());
         int n = nums.size();
         vector<vector<int>> ans;
         for (int i=0; i<n; ++i){
             if (i > 0 && nums[i] == nums[i-1])
                 continue;
             if (nums[i] > 0) break;
-            int l = i+1, r = n-1;
-            while (l < r){
-                int s = nums[i] + nums[l] + nums[r];
+            int l = i+1;
+            int r = n-1;
+            while (l<r){
+                int s = nums[i]+nums[l]+nums[r];
                 if (s == 0){
                     ans.push_back({nums[i],nums[l],nums[r]});
-                    ++l;
-                    --r;
-                    while (l < r && nums[l] == nums[l-1]) ++l;
-                    while (l < r && nums[r] == nums[r+1]) --r;
-                } else if (s < 0)
-                    ++l;
-                else 
-                    --r;
+                    l++;
+                    r--;
+                    while (l<r && nums[l] == nums[l-1])
+                        l++;
+                    while (l<r && nums[r] == nums[r+1])
+                        r--;
+                } else if (s < 0){
+                    l++;
+                } else
+                    r--;
             }
         }
         return ans;
