6f33d2de485ba684a3fae29cbae5078c2a361e0e
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-04-02 22:00:50 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -4,32 +4,25 @@ class Solution:
         ans = []
         for account in accounts:
             name,rest = account[0],set(account[1:])
-            #print('cur:',account)
             if not name in mp:
                 mp[name] = []
                 mp[name].append(rest)
                 continue
             updatedEmails = []
             toRemove = []
+            allDisjoint = True
             for emails in mp[name]:
-                #print('emails:',emails)
                 if not rest.isdisjoint(emails):
                     updatedEmails.append(emails.union(rest))
                     toRemove.append(emails)
+                    allDisjoint = False
+            sz = 0
             for emails in toRemove:
                 mp[name].remove(emails)
-            n = len(updatedEmails)
-            if n == 0:
-                mp[name].append(rest)
-            elif n == 1:
-                mp[name].append(updatedEmails[0])
-            else:
-                #print('UE:',updatedEmails)
-                reduced = reduce(lambda s,t: s.union(t),updatedEmails,set())
-                #print('reduced:',reduced)
-                mp[name].append(reduced)
-            #print('mp:',mp)
-        #print('mp:',mp)
+                sz += 1
+            mp[name].append(reduce(lambda s,t: s.union(t),updatedEmails[1:] if sz > 1 else set(),updatedEmails[0])
+                            if not allDisjoint else rest)
+
         for name,emailLists in mp.items():
             for emailList in emailLists:
                 ans.append([name]+sorted(emailList))
