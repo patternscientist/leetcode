696da9299fbb18244b9c0c5d04748687f1314814
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-04-14 19:44:02 -0400

    Time: 2 ms (57.57%), Space: 7.9 MB (70.71%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -5,13 +5,16 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int l = 1, r = n;
+        int ans = -1;
         while (l < r){
             int mid = l + (r-l) / 2;
-            if (isBadVersion(mid))
-                r = mid;
-            else
-                l = mid + 1;
+            if (isBadVersion(mid)){
+                ans = mid;
+                r   = mid;
+                //cout << "mid: " << mid << endl;
+            } else 
+                l   = mid+1;
         }
-        return l;
+        return ans == -1 ? l : ans;
     }
 };
