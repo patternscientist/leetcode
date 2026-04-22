331e94707a468a2a2f9708dce92435ee07e447b2
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-04-21 20:37:27 -0400

    Time: 3 ms (87.61%), Space: 20.5 MB (67.5%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,21 +1,21 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = [[] for _ in range(numCourses)]
+        adj   = [[] for _ in range(numCourses)]
         indeg = [0]*numCourses
         for preq in prerequisites:
-            a,b = preq
-            adj[b].append(a)
-            indeg[a] += 1
+            a_i,b_i = preq
+            adj[b_i].append(a_i)
+            indeg[a_i] += 1
         q = deque()
-        for course,deg in enumerate(indeg):
-            if deg == 0:
+        for course in range(numCourses):
+            if indeg[course] == 0:
                 q.append(course)
         taken = 0
         while len(q) != 0:
-            course = q.popleft()
+            cur = q.popleft()
             taken += 1
-            for nextCourse in adj[course]:
-                indeg[nextCourse] -= 1
-                if indeg[nextCourse] == 0:
-                    q.append(nextCourse)
+            for other in adj[cur]:
+                indeg[other] -= 1
+                if indeg[other] == 0:
+                    q.append(other)
         return taken == numCourses
