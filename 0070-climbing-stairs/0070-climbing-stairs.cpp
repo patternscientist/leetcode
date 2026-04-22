7d24c6eba5e5187e1c2eaa35dbb8f49328d29554
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-04-21 20:45:18 -0400

    Time: 0 ms (100%), Space: 7.9 MB (69.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int a = 1; // ways to climb 0
-        int b = 1; // ways to climb 1
-        for (int k=2; k<=n; ++k){
-            int c = a+b; // ways to get k
-            // shift window 
+        int a = 1, b = 1;
+        for (int k=2; k<=n; k++){
+            int c = a+b;
             a = b;
             b = c;
         }
