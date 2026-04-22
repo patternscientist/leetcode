345d3612ab1d668a9b6fe1ceef26995b67adb9b0
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-04-21 23:55:50 -0400

    Time: 154 ms (5.01%), Space: 19.3 MB (66.92%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        vector<int> indeg(numCourses);
-        for (const vector<int>& preq : prerequisites){
-            int a_i = preq[0], b_i = preq[1];
+        vector<vector<int>> adj(numCourses, vector<int>(0, 0));
+        vector<int> indeg(numCourses,0);
+        for (const vector<int>& prereq : prerequisites){
+            int a_i = prereq[0];
+            int b_i = prereq[1];
             adj[b_i].push_back(a_i);
             indeg[a_i]++;
+            cout << "a_i: " << a_i << endl;
+            cout << "indeg[a_i]: " << indeg[a_i] << endl;
         }
         queue<int> q;
-        for (int c=0; c<numCourses; c++){
-            if (indeg[c] == 0)
-                q.push(c);
+        for (int course=0; course<numCourses; course++){
+            if (indeg[course] == 0)
+                q.push(course);
         }
         int taken = 0;
         while (!q.empty()){
             int cur = q.front(); q.pop();
+            cout << "cur: " << cur << endl;
             taken++;
-            for (int c : adj[cur]){
-                if (--indeg[c] == 0)
-                    q.push(c);
+            for (int other : adj[cur]){
+                if (--indeg[other] == 0)
+                    q.push(other);
             }
         }
+        cout << "taken: " << taken << endl;
         return taken == numCourses;
     }
 };
