ea39eaae449b270452e85e8ebc72bbe2904dcc83
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-04-21 20:11:29 -0400

    Time: 43 ms (97.66%), Space: 19.6 MB (94.96%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,19 +1,19 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        m,n = len(s),len(t)
+        m, n = len(s), len(t)
         if m < n:
             return ""
-        need = [0]*128
+        need = [0]*256
         for ch in t:
             need[ord(ch)] += 1
         missing = n
+        l = 0
         INF = int(1e5)+1
         bestLen = INF
         bestStart = 0
-        l = 0
         for r in range(m):
             cr = s[r]
-            if need[ord(cr)] > 0:
+            if (need[ord(cr)] > 0):
                 missing -= 1
             need[ord(cr)] -= 1
             while missing == 0:
@@ -23,7 +23,7 @@ class Solution:
                     bestStart = l
                 cl = s[l]
                 need[ord(cl)] += 1
-                if need[ord(cl)] > 0:
+                if (need[ord(cl)] > 0):
                     missing += 1
                 l += 1
         return "" if bestLen == INF else s[bestStart:bestStart+bestLen]
