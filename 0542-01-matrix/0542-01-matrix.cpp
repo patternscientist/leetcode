816897750758ea227bf5aa7bbea89231f9949fe9
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-04-22 16:56:29 -0400

    Time: 7 ms (92.96%), Space: 34.8 MB (62.13%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
-        vector<vector<int>> dist(m,vector<int>(n,-1));
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> dist(m, vector<int>(n, -1));
         queue<pair<int,int>> q;
-        for (int r=0; r<m; ++r){
-            for (int c=0; c<n; ++c){
+        for (int r=0; r<m; r++){
+            for (int c=0; c<n; c++){
                 if (mat[r][c] == 0){
                     dist[r][c] = 0;
                     q.push({r,c});
                 }
             }
         }
-        int dr[4] = {0,1,0,-1};
-        int dc[4] = {1,0,-1,0};
+        int dr[4] = {0,-1,0,1};
+        int dc[4] = {-1,0,1,0};
         while (!q.empty()){
             auto [r,c] = q.front(); q.pop();
-            for (int k=0; k<4; ++k){
-                int nr = r+dr[k];
-                int nc = c+dc[k];
-                if (0 <= nr && nr < m && 
+            for (int k=0; k<4; k++){
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+                if (0 <= nr && nr < m &&
                     0 <= nc && nc < n &&
                     dist[nr][nc] == -1){
                         dist[nr][nc] = dist[r][c]+1;
