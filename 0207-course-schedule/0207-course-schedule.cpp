9c33144224f4885d9678b45f968ffeb45f47984c
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-04-21 23:57:14 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -8,8 +8,6 @@ public:
             int b_i = prereq[1];
             adj[b_i].push_back(a_i);
             indeg[a_i]++;
-            cout << "a_i: " << a_i << endl;
-            cout << "indeg[a_i]: " << indeg[a_i] << endl;
         }
         queue<int> q;
         for (int course=0; course<numCourses; course++){
@@ -19,14 +17,12 @@ public:
         int taken = 0;
         while (!q.empty()){
             int cur = q.front(); q.pop();
-            cout << "cur: " << cur << endl;
             taken++;
             for (int other : adj[cur]){
                 if (--indeg[other] == 0)
                     q.push(other);
             }
         }
-        cout << "taken: " << taken << endl;
         return taken == numCourses;
     }
 };
