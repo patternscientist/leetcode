f896b8626455c2d375a01aecdee5b602dfb44e25
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-04-24 15:30:20 -0400

    Time: 35 ms (99.01%), Space: 29.2 MB (28.13%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(),nums.end());
         int n = nums.size();
         vector<vector<int>> ans;
-        for (int i=0; i<n; ++i){
+        for (int i=0; i<n; i++){
             if (nums[i] > 0)
                 break;
             if (i > 0 && nums[i] == nums[i-1])
@@ -19,7 +19,7 @@ public:
                     r--;
                     while (l < r && nums[l] == nums[l-1])
                         l++;
-                    while (l < r && nums[r] == nums[r+1])
+                    while (l < r && nums[r] == nums[r+1])   
                         r--;
                 } else if (s < 0)
                     l++;
