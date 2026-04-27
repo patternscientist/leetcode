bd8c576b5dc8d6f69c798e7e7041a3ac75fbe573
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-04-27 14:53:59 -0700

    Time: 2 ms (36.23%), Space: 10.9 MB (31.45%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,26 +2,25 @@ class Solution {
 public:
     vector<vector<int>> ans;
     vector<int> path;
-    void dfs(const vector<int>& nums, const int n, vector<bool>& used){
-        if (path.size() == n){ // checking if all of used was true w/ all_of was slower than this (one the 1 test I did)
+    void dfs(const vector<int>& nums, vector<int>& used, int n, int remaining){
+        if (remaining == 0){
             ans.push_back(path);
             return;
         }
-        for (int i=0; i<n; ++i){
-            if (used[i]) continue;
-            used[i] = true;
-            int x = nums[i];
-            path.push_back(x);
-            dfs(nums,n,used);
+        for (int i=0; i<n; i++){
+            if (used[i])
+                continue;
+            used[i] = 1;
+            path.push_back(nums[i]);
+            dfs(nums,used,n,remaining-1);
             path.pop_back();
-            used[i] = false;
+            used[i] = 0;
         }
-        return;
     }
     vector<vector<int>> permute(vector<int>& nums) {
         int n = nums.size();
-        vector<bool> used(n,false);
-        dfs(nums,n,used);
+        vector<int> used(n,0);
+        dfs(nums,used,n,n);
         return ans;
     }
 };
