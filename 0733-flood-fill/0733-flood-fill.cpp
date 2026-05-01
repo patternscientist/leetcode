27b9e766ce9470e12fb89c79b65eb7038a101f20
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-04-30 23:20:13 -0400

    Time: 0 ms (100%), Space: 18.2 MB (43.27%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int og = image[sr][sc];
-        if (og == color) return image;
-        
-        int m = image.size(), n = image.size();
+        if (image[sr][sc] == color)
+            return image;
+        int ogColor = image[sr][sc];
+        int m = image.size(); 
+        int n = image[0].size();
         queue<pair<int,int>> q;
         image[sr][sc] = color;
         q.push({sr,sc});
-
-        int dr[4] = {0,-1,0,1};
-        int dc[4] = {-1,0,1,0};
-        int nr, nc;
-
+        int dr[4] = {0,1,0,-1};
+        int dc[4] = {1,0,-1,0};
         while (!q.empty()){
             auto [r,c] = q.front(); q.pop();
-            for (int k=0; k<4; ++k){
-                nr = r + dr[k];
-                nc = c + dc[k];
+            for (int k=0; k<4; k++){
+                int nr = r + dr[k];
+                int nc = c + dc[k];
                 if (0 <= nr && nr < m &&
                     0 <= nc && nc < n &&
-                    image[nr][nc] == og){
+                    image[nr][nc] == ogColor){
                         image[nr][nc] = color;
                         q.push({nr,nc});
                     }
             }
         }
-
         return image;
     }
 };
