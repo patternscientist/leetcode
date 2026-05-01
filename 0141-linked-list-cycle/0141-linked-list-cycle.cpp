a1d127da0b9f7b15ff91e305d94c60cad3dfbb9f
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-05-01 16:12:28 -0400

    Time: 12 ms (43.37%), Space: 11.9 MB (51.78%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -14,7 +14,8 @@ public:
         while (fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
-            if (slow == fast) return true;
+            if (slow == fast)
+                return true;
         }
         return false;
     }
