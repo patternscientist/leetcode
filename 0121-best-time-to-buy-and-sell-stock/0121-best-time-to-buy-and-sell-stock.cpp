3414c7639027796952131505e338659a7175cda0
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-05-10 01:43:44 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int best    = 0;
-        int bottom  = INT_MAX;
+        int bottom  = prices[0];
         for (int price : prices){
             bottom = min(bottom, price);
             best   = max(best, price-bottom);
