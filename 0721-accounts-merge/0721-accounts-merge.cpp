bd16ab00e52709850bfcfc6aa898c04945cf4399
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-05-09 22:42:56 -0400

    Time: 27 ms (81.83%), Space: 45.2 MB (30.1%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -0,0 +1,79 @@
+class Solution {
+struct DSU{
+    vector<int> parent;
+    vector<int> rank;
+
+    DSU(int n){
+        parent.resize(n);
+        rank.assign(n,0);
+
+        for (int i=0; i<n; i++)
+            parent[i] = i;
+    }
+
+    int find(int x){
+        if (parent[x] == x)
+            return x;
+        parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    void unite(int x, int y){
+        int rootX = find(x);
+        int rootY = find(y);    
+        if (rootX == rootY)
+            return;
+        if (rank[rootX] > rank[rootY])
+            parent[rootY] = rootX;
+        else if (rank[rootX] < rank[rootY])
+            parent[rootX] = rootY;
+        else{
+            parent[rootY] = rootX;
+            rank[rootX]++; 
+        }
+    }
+};
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        unordered_map<string,int> emailToId;
+        unordered_map<string,string> emailToName;
+        int id = 0;
+
+        for (const auto& account : accounts){
+            string name = account[0];
+            for (int i=1; i<(int)account.size(); i++){
+                string email = account[i];
+                if (!emailToName.count(email))
+                    emailToName[email] = name;
+                emailToId[email] = id;
+            }
+            id++;
+        }
+
+        DSU dsu(id);
+        for (const auto& account : accounts){
+            int firstEmailId = emailToId[account[1]];
+            for (int i=2; i<(int)account.size(); i++){
+                dsu.unite(firstEmailId, emailToId[account[i]]);
+            }
+        }
+
+        unordered_map<int,vector<string>> rootToEmails;
+        for (const auto& [email, emailId] : emailToId){
+            int root = dsu.find(emailId);
+            rootToEmails[root].push_back(email);
+        }
+
+        vector<vector<string>> ans;
+        for (auto& [root, emails] : rootToEmails){
+            sort(emails.begin(), emails.end());
+            vector<string> merged;
+            merged.push_back(emailToName[emails[0]]);
+            for (const string& email : emails)
+                merged.push_back(email);
+            ans.push_back(merged);
+        }
+
+        return ans;
+    }
+};
