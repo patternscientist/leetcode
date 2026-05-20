0858f2e321626d41f0677a4c4a8a589d2f8b0c84
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-05-19 20:13:48 -0400

    Time: 3 ms (44.65%), Space: 19.4 MB (29.09%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,9 +1,9 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         ans = 0
-        count = [0] * 128
+        count = [0] * 60
         for ch in s:
-            count[ord(ch)] += 1
+            count[ord(ch)-ord('a')] += 1
         hasOdd = False
         for x in count:
             ans += (x // 2) * 2
