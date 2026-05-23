178ff4c1c715e3db31608127f0301d3e5a3deb7d
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-05-22 22:16:18 -0400

    Time: 0 ms (100%), Space: 9 MB (97.91%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -3,29 +3,24 @@ public:
     int myAtoi(string s) {
         int n = (int)s.size();
         int i = 0;
-        long long ans = 0;
-        bool positive = true;
-        while (s[i] == ' ') i++;
-        if (s[i] == '-'){
-            positive = false;
-            i++;
-        } else if (s[i] == '+')
-            i++;
-        while (s[i] == '0') i++;
-        while (i < n && isdigit(s[i])){
-            if (ans > INT_MAX/10){
-                ans = positive ? INT_MAX : INT_MIN;
-                return ans;
-            }
-            ans *= 10;
-            ans += (long long)s[i]-'0';
+        // skip whitespace
+        while (i < n && s[i] == ' ') i++;
+        // account for sign
+        int sign = 1;
+        if (i < n && (s[i] == '-' || s[i] == '+')){
+            if (s[i] == '-')
+                sign = -1;
             i++;
         }
-        if (!positive){
-            ans *= -1;
-            ans = max(ans,(long long)INT_MIN);
-        } else
-            ans = min(ans,(long long)INT_MAX);
-        return ans;
+        // process digits, clamping before overflow
+        int ans = 0;
+        while (i < n && isdigit(s[i])){
+            int digit = s[i]-'0';
+            if (ans > (INT_MAX - digit) / 10)
+                return sign == 1 ? INT_MAX : INT_MIN;
+            ans = ans * 10 + digit;
+            i += 1;
+        }
+        return sign * ans;
     }
 };
