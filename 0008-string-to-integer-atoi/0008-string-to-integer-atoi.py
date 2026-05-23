58b26fd5122a85247446d7efc44a001bb934a16f
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-05-22 21:54:09 -0400

    Time: 3 ms (36.35%), Space: 19.3 MB (42.81%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -2,25 +2,22 @@ class Solution:
     def myAtoi(self, s: str) -> int:
         n = len(s)
         i = 0
-
-        # skip leading whitespace
+        # skip whitespace
         while i < n and s[i] == ' ':
             i += 1
-
-        # optional sign
+        # account for sign
         sign = 1
-        if i < n and s[i] in {'+','-'}:
+        if i < n and (s[i] == '+' or s[i] == '-'):
             if s[i] == '-':
                 sign = -1
             i += 1
-        
-        # build ans, clamping before overflow
+        # build answer, clamping before overflow
         MAX = int(2**31)-1
         MIN = -int(2**31)
         ans = 0
         while i < n and s[i].isdigit():
             digit = int(s[i])
-            if ans > ((MAX - digit)//10):
+            if ans > (MAX - digit) // 10:
                 return MAX if sign == 1 else MIN
             ans = ans * 10 + digit
             i += 1
