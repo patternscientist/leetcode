72d1cc33449ecbe9c772241eeaec3db3002f7215
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-05-25 23:40:20 -0400

    Time: 0 ms (100%), Space: 15.1 MB (21.34%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -14,56 +14,50 @@ public:
     vector<int> rightSideView(TreeNode* root) {
         if (!root)
             return {};
-        queue<TreeNode*> q;
-        if (root->left){
-            q.push(root->left);
-        }
+
         vector<int> ans;
         ans.push_back(root->val);
-        queue<TreeNode*> q2;
+
+        queue<TreeNode*> qRight;
         if (root->right)
-            q2.push(root->right);
+            qRight.push(root->right);
+
+        queue<TreeNode*> qLeft;
+        if (root->left)
+            qLeft.push(root->left);
+        
         int maxRightDepth = 0;
-        while (!q2.empty()){
+        while (!qRight.empty()){
             maxRightDepth++;
-            int sz = (int)q2.size();
+            int sz = (int)qRight.size();
             for (int i=0; i<sz; i++){
-                TreeNode* cur = q2.front();
-                q2.pop();
+                TreeNode* cur = qRight.front();
+                qRight.pop();
                 if (i == sz-1)
                     ans.push_back(cur->val);
                 if (cur->left)
-                    q2.push(cur->left);
+                    qRight.push(cur->left);
                 if (cur->right)
-                    q2.push(cur->right);
+                    qRight.push(cur->right);
             }
         }
 
-        // root = root->right;
-        // while (root){
-        //     maxRightDepth++;
-        //     ans.push_back(root->val);
-        //     root = root->right;
-        // }
-
         int leftDepth = 1;
-        while (!q.empty()){
-            int sz = (int)q.size();
+        while (!qLeft.empty()){
+            int sz = (int)qLeft.size();
             for (int i=0; i<sz; i++){
-                TreeNode* cur = q.front();
-                q.pop();
-                // cout << "cur val: " << cur->val << endl;
-                // cout << "cur i: " << i << endl;
-                // cout << "sz: " << sz << endl;
+                TreeNode* cur = qLeft.front();
+                qLeft.pop();
                 if (i == sz-1 && leftDepth > maxRightDepth)
                     ans.push_back(cur->val);
                 if (cur->left) 
-                    q.push(cur->left);
+                    qLeft.push(cur->left);
                 if (cur->right)
-                    q.push(cur->right);
+                    qLeft.push(cur->right);
             }
             leftDepth++;
         }
+
         return ans;
     }
 };
