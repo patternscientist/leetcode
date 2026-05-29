1c948321cee1e6e599503b5582a60b9f265e2577
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-05-29 17:55:40 -0400

    Time: 0 ms (100%), Space: 14.9 MB (63.57%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -14,50 +14,21 @@ public:
     vector<int> rightSideView(TreeNode* root) {
         if (!root)
             return {};
-
         vector<int> ans;
-        ans.push_back(root->val);
-
-        queue<TreeNode*> qRight;
-        if (root->right)
-            qRight.push(root->right);
-
-        queue<TreeNode*> qLeft;
-        if (root->left)
-            qLeft.push(root->left);
-        
-        int maxRightDepth = 0;
-        while (!qRight.empty()){
-            maxRightDepth++;
-            int sz = (int)qRight.size();
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()){
+            int sz = (int)q.size();
             for (int i=0; i<sz; i++){
-                TreeNode* cur = qRight.front();
-                qRight.pop();
+                TreeNode* cur = q.front(); q.pop();
                 if (i == sz-1)
                     ans.push_back(cur->val);
                 if (cur->left)
-                    qRight.push(cur->left);
+                    q.push(cur->left);
                 if (cur->right)
-                    qRight.push(cur->right);
+                    q.push(cur->right);
             }
         }
-
-        int leftDepth = 1;
-        while (!qLeft.empty()){
-            int sz = (int)qLeft.size();
-            for (int i=0; i<sz; i++){
-                TreeNode* cur = qLeft.front();
-                qLeft.pop();
-                if (i == sz-1 && leftDepth > maxRightDepth)
-                    ans.push_back(cur->val);
-                if (cur->left) 
-                    qLeft.push(cur->left);
-                if (cur->right)
-                    qLeft.push(cur->right);
-            }
-            leftDepth++;
-        }
-
         return ans;
     }
 };
