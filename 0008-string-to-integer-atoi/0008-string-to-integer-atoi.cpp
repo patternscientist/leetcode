df985181a6e1934c49f5210718e6f95492f7e5d9
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-06-03 01:16:50 -0400

    Time: 0 ms (100%), Space: 9.3 MB (10.57%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -3,23 +3,23 @@ public:
     int myAtoi(string s) {
         int n = (int)s.size();
         int i = 0;
-        // skip whitespace
-        while (i < n && s[i] == ' ') i++;
-        // account for sign
+        while (i < n && s[i] == ' ')
+            i++;
         int sign = 1;
         if (i < n && (s[i] == '-' || s[i] == '+')){
             if (s[i] == '-')
                 sign = -1;
             i++;
         }
-        // process digits, clamping before overflow
         int ans = 0;
+        int MAX = INT_MAX;
+        int MIN = INT_MIN;
         while (i < n && isdigit(s[i])){
             int digit = s[i]-'0';
-            if (ans > (INT_MAX - digit) / 10)
-                return sign == 1 ? INT_MAX : INT_MIN;
+            if ((INT_MAX - digit) / 10 < ans)
+                return sign == 1 ? MAX : MIN;
             ans = ans * 10 + digit;
-            i += 1;
+            i++;
         }
         return sign * ans;
     }
