4be638422e0b4366dfac44464c8a5695c4678974
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-06-03 20:08:51 -0400

    Time: 0 ms (100%), Space: 9.3 MB (80.09%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -3,29 +3,26 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int m = (int)matrix.size();
         int n = (int)matrix[0].size();
-        vector<int> ans;
         int rMin = 0, rMax = m-1;
         int cMin = 0, cMax = n-1;
+        vector<int> ans;
         while (rMin <= rMax && cMin <= cMax){
             for (int c=cMin; c<=cMax; c++)
                 ans.push_back(matrix[rMin][c]);
             rMin++;
-            if (rMin > rMax)
-                break;
+            if (rMin > rMax) break;
             for (int r=rMin; r<=rMax; r++)
                 ans.push_back(matrix[r][cMax]);
             cMax--;
-            if (cMin > cMax)
-                break;
-            for (int c=cMax; c>=cMin; c--) 
+            if (cMin > cMax) break;
+            for (int c=cMax; c>=cMin; c--)
                 ans.push_back(matrix[rMax][c]);
             rMax--;
-            if (rMin > rMax)
-                break;
+            if (rMin > rMax) break;
             for (int r=rMax; r>=rMin; r--)
                 ans.push_back(matrix[r][cMin]);
             cMin++;
         }
         return ans;
-    }
+    }   
 };
