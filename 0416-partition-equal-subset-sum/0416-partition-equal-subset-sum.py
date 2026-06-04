2393e2ec4c693359f7e63d268c495c8e35eadcbe
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-06-04 15:17:01 -0400

    Time: 646 ms (64.48%), Space: 19.3 MB (71.22%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -4,7 +4,7 @@ class Solution:
         if total % 2 == 1:
             return False
         target = total // 2
-        dp = [False] * (target+1)
+        dp = [False] * (target + 1)
         dp[0] = True
         for x in nums:
             for s in range(target,x-1,-1):
