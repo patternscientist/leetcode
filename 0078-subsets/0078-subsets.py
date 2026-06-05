133c66b93f3cb2abe2c5a058effd5ad6d1ee6f7a
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-06-05 15:16:30 -0400

    Time: 0 ms (100%), Space: 19.5 MB (28.82%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,15 +1,14 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        path = []
         ans  = []
-        def dfs(nums: List[int], start: int) -> None:
+        def dfs(nums: List[int], path: List[int], start: int) -> None:
             ans.append(path[:])
             if start == n:
                 return 
             for i in range(start,n):
                 path.append(nums[i])
-                dfs(nums,i+1)
+                dfs(nums,path,i+1)
                 path.remove(nums[i])
-        dfs(nums,0)
+        dfs(nums,[],0)
         return ans
