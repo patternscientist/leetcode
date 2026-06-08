7d6a719033baecd048742126be0b312ef3a39c0c
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-06-08 11:19:24 -0400

    Time: 24 ms (77.62%), Space: 18.3 MB (38.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,23 +3,23 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int m = (int)grid.size();
         int n = (int)grid[0].size();
-        int dr[4] = {0,-1,0,1};
-        int dc[4] = {-1,0,1,0};
-        queue<pair<int,int>> q;
         int islands = 0;
-        for (int r=0; r<m; ++r){
-            for (int c=0; c<n; ++c){
+        queue<pair<int,int>> q;
+        int dr[4] = {-1,0,1,0};
+        int dc[4] = {0,1,0,-1};
+        for (int r=0; r<m; r++){
+            for (int c=0; c<n; c++){
                 if (grid[r][c] != '1')
                     continue;
-                islands++;
                 grid[r][c] = 'X';
                 q.push({r,c});
                 while (!q.empty()){
-                    auto [curR, curC] = q.front(); q.pop();
-                    for (int k=0; k<4; ++k){
-                        int nr = curR + dr[k];
-                        int nc = curC + dc[k];
-                        if (0 <= nr && nr < m && 
+                    auto [R,C] = q.front();
+                    q.pop();
+                    for (int k=0; k<4; k++){
+                        int nr = R + dr[k];
+                        int nc = C + dc[k];
+                        if (0 <= nr && nr < m &&
                             0 <= nc && nc < n &&
                             grid[nr][nc] == '1'){
                                 grid[nr][nc] = 'X';
@@ -27,6 +27,7 @@ public:
                             }
                     }
                 }
+                islands++;
             }
         }
         return islands;
