aa3611ac9fe92ccba26d55c0d331a7bd70b4847e
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-06-10 23:14:36 -0400

    Time: 132 ms (5%), Space: 52.7 MB (5.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,12 +5,12 @@ public:
             return x[0] < y[0];
         });
         vector<vector<int>> merged;
-        for (int i=0; i<(int)intervals.size(); i++){
-            if (merged.empty() || merged.back()[1] < intervals[i][0])
-                merged.push_back(intervals[i]);
+        for (const auto& interval : intervals){
+            if (merged.empty() || merged.back()[1] < interval[0])
+                merged.push_back(interval);
             else{
-                merged.back()[0] = min(merged.back()[0], intervals[i][0]);
-                merged.back()[1] = max(merged.back()[1], intervals[i][1]);
+                merged.back()[0] = min(merged.back()[0], interval[0]);
+                merged.back()[1] = max(merged.back()[1], interval[1]);
             }
         }
         return merged;
