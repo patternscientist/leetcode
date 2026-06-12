86943fa1e95b006f3e28d052145e78638e0dc1b3
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-06-12 19:08:51 -0400

    Time: 0 ms (100%), Space: 15.3 MB (33.68%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,20 +4,20 @@ public:
         int n = (int)nums.size();
         int l = 0;
         int r = n-1;
-        while (l<=r){
-            int mid = l+(r-l)/2;
+        while (l <= r){
+            int mid = l + (r-l) / 2;
             if (nums[mid] == target)
                 return mid;
-            if (nums[l] <= nums[mid]){
+            else if (nums[l] <= nums[mid]){
                 if (nums[l] <= target && target < nums[mid])
-                    r = mid-1;
+                    r--;
                 else 
-                    l = mid+1;
+                    l++;
             } else {
                 if (nums[mid] < target && target <= nums[r])
-                    l = mid+1;
-                else
-                    r = mid-1;
+                    l++;
+                else 
+                    r--;
             }
         }
         return -1;
