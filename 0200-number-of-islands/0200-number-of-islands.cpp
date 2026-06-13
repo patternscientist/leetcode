96a74c7088dea6df41e373a10679738c18a68f11
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-06-13 00:53:12 -0400

    Time: 27 ms (55.66%), Space: 18.4 MB (38.05%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,33 +3,33 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int m = (int)grid.size();
         int n = (int)grid[0].size();
-        int islands = 0;
         queue<pair<int,int>> q;
         int dr[4] = {-1,0,1,0};
         int dc[4] = {0,1,0,-1};
+        int islands = 0;
         for (int r=0; r<m; r++){
             for (int c=0; c<n; c++){
                 if (grid[r][c] != '1')
                     continue;
-                grid[r][c] = 'X';
+                grid[r][c] = '1';
                 q.push({r,c});
                 while (!q.empty()){
-                    auto [R,C] = q.front();
+                    auto [r,c] = q.front();
                     q.pop();
                     for (int k=0; k<4; k++){
-                        int nr = R + dr[k];
-                        int nc = C + dc[k];
+                        int nr = r+dr[k];
+                        int nc = c+dc[k];
                         if (0 <= nr && nr < m &&
-                            0 <= nc && nc < n &&
+                            0 <= nc && nc < n && 
                             grid[nr][nc] == '1'){
                                 grid[nr][nc] = 'X';
                                 q.push({nr,nc});
                             }
+                        }
                     }
+                    islands++;
                 }
-                islands++;
             }
-        }
         return islands;
     }
 };
