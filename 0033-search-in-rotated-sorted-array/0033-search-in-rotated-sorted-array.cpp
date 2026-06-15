337c944775f5e5147502b3acb40af97706abae88
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-06-15 11:25:32 -0400

    Time: 0 ms (100%), Space: 15.1 MB (96.05%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -10,14 +10,14 @@ public:
                 return mid;
             else if (nums[l] <= nums[mid]){
                 if (nums[l] <= target && target < nums[mid])
-                    r--;
+                    r = mid-1;
                 else 
-                    l++;
+                    l = mid+1;
             } else {
                 if (nums[mid] < target && target <= nums[r])
-                    l++;
+                    l = mid+1;
                 else 
-                    r--;
+                    r = mid-1;
             }
         }
         return -1;
