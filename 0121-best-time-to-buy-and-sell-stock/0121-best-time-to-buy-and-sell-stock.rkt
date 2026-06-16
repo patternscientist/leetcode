84b4d814f01e4ed002f37f7521e52cb2d45aea80
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-06-16 06:21:09 -0400

    Time: 8 ms (50%), Space: 134.4 MB (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.rkt
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.rkt
@@ -1,8 +1,11 @@
 (define/contract (max-profit prices)
-  (-> (listof exact-integer?) exact-integer?)
-  (for/fold ([bottom (first prices)] [best 0] #:result best)
-            ([price (rest prices)])
-        (let* ([bottomNext (min bottom price)] [bestNext (max best (- price bottomNext))])
-            (values bottomNext bestNext))
-            )
+  (-> (cons/c exact-integer? (listof exact-integer?)) exact-integer?)
+  (define first-price (first prices))
+  (for/fold ([best 0]
+             [bottom first-price]
+             #:result best) 
+             ([price (in-list (rest prices))])
+        (define next-bottom (min bottom price))
+        (define next-best (max best (- price next-bottom)))
+        (values next-best next-bottom))
   )
