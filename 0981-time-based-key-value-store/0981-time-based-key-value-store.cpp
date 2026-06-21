1c17185975db814da32f7c9378e177b2cee7e043
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-06-21 07:14:43 -0400

    Time: 57 ms (41.57%), Space: 136.5 MB (82.49%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -1,9 +1,8 @@
 class TimeMap {
 public:
-    unordered_map<string, vector<pair<int,string>>> mp;
+    unordered_map<string,vector<pair<int,string>>> mp;
 
     TimeMap() {
-        
     }
     
     void set(string key, string value, int timestamp) {
@@ -19,14 +18,14 @@ public:
         int r = (int)vec.size()-1;
         int ans = -1;
         while (l <= r){
-            int mid = l + (r-l) / 2;
+            int mid = l+(r-l)/2;
             if (vec[mid].first <= timestamp){
                 ans = mid;
                 l   = mid+1;
             } else 
-                r   = mid-1;
+                r = mid-1;
         }
-        return (ans == -1) ? "" : vec[ans].second;
+        return ans == -1 ? "" : vec[ans].second;
     }
 };
 
