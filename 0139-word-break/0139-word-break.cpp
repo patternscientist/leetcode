eca4dacc5e83bbec013ea95671da2557dd8488c1
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-06-29 04:08:50 -0400

    Time: 0 ms (100%), Space: 10.6 MB (86.83%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> wordSet(wordDict.begin(),
-                                      wordDict.end());
-        int n = (int)s.size();
+        unordered_set<string> wordSet(wordDict.begin(),wordDict.end());
         int maxLen = 0;
         for (const string& w : wordDict)
-            maxLen = max(maxLen,(int)w.size());
+            maxLen = max(maxLen, (int)w.size());
+        int n = (int)s.size();
         vector<bool> dp(n+1,false);
         dp[0] = true;
         for (int i=1; i<=n; i++){
             for (int len=1; len<=min(i,maxLen); len++){
-                int j=i-len;
+                int j = i-len;
                 if (dp[j] && wordSet.contains(s.substr(j,len))){
                     dp[i] = true;
                     break;
