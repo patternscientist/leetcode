b6ec83ed36dae3e91c6ebedbde6a6a737398b6f5
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-07-05 19:11:52 -0700

    Time: 7 ms (22.59%), Space: 11.9 MB (80.86%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -24,17 +24,19 @@ public:
     Node* cloneGraph(Node* node) {
         if (!node)
             return nullptr;
-        unordered_map<Node*,Node*> mp{{node,new Node(node->val)}};
+        unordered_map<Node*,Node*> mp;
+        mp[node] = new Node(node->val);
         queue<Node*> q;
         q.push(node);
         while (!q.empty()){
-            Node* cur = q.front(); q.pop();
-            for (Node* neighbor : cur->neighbors){
-                if (!mp.count(neighbor)){
-                    mp[neighbor] = new Node(neighbor->val);
-                    q.push(neighbor);
+            Node* cur = q.front();
+            q.pop();
+            for (Node* nb : cur->neighbors){
+                if (!mp.contains(nb)){
+                    mp[nb] = new Node(nb->val);
+                    q.push(nb);
                 }
-                mp[cur]->neighbors.push_back(mp[neighbor]);
+                mp[cur]->neighbors.push_back(mp[nb]);
             }
         }
         return mp[node];
