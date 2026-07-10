0962be019aa078e8e193c3747cea8f288409f815
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-07-09 22:54:18 -0700

    Time: 41 ms (97.76%), Space: 19.7 MB (73.04%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,34 +1,34 @@
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
-        m,n  = len(s),len(t)
-        if n > m:
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        m,n = len(s),len(t)
+        if m < n:
             return ""
-        need = [0] * 58
+        need = [0] * 256
         for ch in t:
-            need[ord(ch)-ord('A')] += 1
-        missing   = n
-        l         = 0
-        INF       = int(1e5)+1
-        bestLen   = INF
+            need[ord(ch)] += 1
+
+        missing = n
+        l = 0
+        INF = int(1e5)+1 # bigger than any possible len
+        bestLen = INF
         bestStart = 0
         for r in range(m):
-            sr = s[r]
-            if need[ord(sr)-ord('A')] > 0:
+            cr = s[r]
+
+            if need[ord(cr)] > 0:
                 missing -= 1
-            need[ord(sr)-ord('A')] -= 1
+            
+            need[ord(cr)] -= 1
+
             while missing == 0:
                 winLen = r-l+1
                 if winLen < bestLen:
-                    bestLen   = winLen
+                    bestLen = winLen
                     bestStart = l
-                sl = s[l]
-                need[ord(sl)-ord('A')] += 1
-                if need[ord(sl)-ord('A')] > 0:
+                cl = s[l]
+                need[ord(cl)] += 1
+                if need[ord(cl)] > 0:
                     missing += 1
                 l += 1
+
         return "" if bestLen == INF else s[bestStart:bestStart+bestLen]
