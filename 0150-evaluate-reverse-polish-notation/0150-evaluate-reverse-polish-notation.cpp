c821eb5ccd5315d0e29b9a545af63a324a68d931
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-07-09 23:18:29 -0700

    Time: 4 ms (19.87%), Space: 17.4 MB (10.44%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        unordered_set<string> op = {"+","-","*","/"};
+        unordered_map<string,function<int(int,int)>> ops =  {{"+", [](int x, int y){ return x+y;}},
+                                                             {"-", [](int x, int y){ return x-y;}},
+                                                             {"*", [](int x, int y){ return x*y;}},
+                                                             {"/", [](int x, int y){ return x/y;}}};
         stack<int> st;
-        for (string token : tokens){
-            if (op.contains(token)){
-                int n2 = st.top(); st.pop();
-                int n1 = st.top(); st.pop();
-                if (token == "+")
-                    st.push(n1+n2);
-                else if (token == "-")
-                    st.push(n1-n2);
-                else if (token == "*")
-                    st.push(n1*n2);
-                else
-                    st.push(n1/n2);
+        for (const string& token : tokens){
+            if (ops.contains(token)){
+                int y = st.top();
+                st.pop();
+                int x = st.top();
+                st.pop();
+                st.push(ops[token](x,y));
             } else 
                 st.push(stoi(token));
         }
