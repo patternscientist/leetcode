75d8757ffc76f3c87f267d5c4660a37574abfb1b
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-07-12 18:01:14 -0700

    Time: 236 ms (79.52%), Space: 21.6 MB (72.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,25 +2,24 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m,n = len(grid),len(grid[0])
         q = deque()
+        dr = [-1,0,1,0]
+        dc = [0,1,0,-1]
         islands = 0
-        dr = (0,1,0,-1)
-        dc = (1,0,-1,0)
         for r in range(m):
             for c in range(n):
-                if grid[r][c] != "1":
+                if grid[r][c] != '1':
                     continue
-                grid[r][c] = "X"
+                grid[r][c] = 'X'
                 q.append((r,c))
-                islands += 1
                 while len(q) != 0:
-                    R,C = q.popleft()
+                    (R,C) = q.popleft()
                     for k in range(4):
-                        nr = R+dr[k]
-                        nc = C+dc[k]
-                        if (0 <= nr and nr < m and
-                            0 <= nc and nc < n and
-                            grid[nr][nc] == "1"):
-                            grid[nr][nc] = "X"
+                        nr = R + dr[k]
+                        nc = C + dc[k]
+                        if (0 <= nr and nr < m and 
+                            0 <= nc and nc < n and 
+                            grid[nr][nc] == '1'):
+                            grid[nr][nc] = 'X'
                             q.append((nr,nc))
+                islands += 1
         return islands
-                
