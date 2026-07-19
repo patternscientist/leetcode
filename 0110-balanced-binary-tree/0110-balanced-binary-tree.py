49bc498b1132ad4c4bfc8929f50b348fd554230d
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-07-19 02:26:30 -0700

    Time: 3 ms (52.5%), Space: 20.3 MB (93.21%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -4,16 +4,20 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-def heightOrFail(root):
-    if not root: return -1
-    lh = heightOrFail(root.left)
-    if lh == -2: return -2
-    rh = heightOrFail(root.right)
-    if rh == -2: return -2
-    if abs(lh-rh) > 1: return -2
-    return 1 + max(lh,rh)
-
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def heightOrFail(root):
+            if not root:
+                return -1
+            if not root.left and not root.right:
+                return 0
+            lh = heightOrFail(root.left)
+            if lh == -2:
+                return -2
+            rh = heightOrFail(root.right)
+            if rh == -2:
+                return -2
+            if abs(lh-rh) > 1:
+                return -2
+            return max(lh,rh)+1
         return heightOrFail(root) != -2
-        
