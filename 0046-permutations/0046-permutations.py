ff802ee14e7fc473b6403ec018665d5ba86ff8a3
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-07-19 03:31:39 -0700

    Time: 1 ms (45.29%), Space: 19.6 MB (33.39%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,21 +1,20 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def dfs(nums,used):
-            if all(used):
-                ans.append(path.copy())
-                return
+        ans  = []
+        path = []
+        def dfs(nums,n,used,numUsed):
+            if numUsed == n:
+                ans.append(path[:])
+                return 
             for i,num in enumerate(nums):
                 if used[i]:
                     continue
-                used[i] = True 
                 path.append(num)
-                dfs(nums,used)
+                used[i] = True
+                dfs(nums,n,used,numUsed+1)
                 path.remove(num)
                 used[i] = False
-            return
-        path = []
-        ans  = []
-        n = len(nums)
-        used = [False]*n
-        dfs(nums,used)
+        n    = len(nums)
+        used = [False] * n
+        dfs(nums,n,used,0)
         return ans
