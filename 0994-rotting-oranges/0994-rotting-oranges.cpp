12b1207ee3bce247e5237f50eaf563b7eb1e2c78
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-08-01 16:40:47 -0700

    Time: 0 ms (100%), Space: 16.7 MB (86.67%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = (int)grid.size(), n = (int)grid[0].size();
-        int fresh = 0;
+        int m = (int)grid.size();
+        int n = (int)grid[0].size();
+        int dr[4] = {-1,0,1,0};
+        int dc[4] = {0,1,0,-1};
         queue<pair<int,int>> q;
-        for (int r=0; r<m; ++r){
-            for (int c=0; c<n; ++c){
+        int minutes = 0;
+        int fresh = 0;
+        for (int r=0; r<m; r++){
+            for (int c=0; c<n; c++){
                 if (grid[r][c] == 2)
                     q.push({r,c});
                 else if (grid[r][c] == 1)
                     fresh++;
             }
         }
-        int dr[4] = {0,-1,0,1};
-        int dc[4] = {-1,0,1,0};
-        int minutes = 0;
-        bool madeRotten;
         while (!q.empty()){
-            auto [r,c] = q.front(); q.pop();
-            madeRotten = false;
-            for (int k=0; k<4; ++k){
-                int nr = r + dr[k];
-                int nc = c + dc[k];
-                if (0 <= nr && nr < m &&
-                    0 <= nc && nc < n && 
-                    grid[nr][nc] == 1){
-                        grid[nr][nc] = 2;
-                        madeRotten = true;
-                        fresh--;
-                        q.push({nr,nc});
-                    }
+            int sz = (int)q.size();
+            bool madeRotten = false;
+            for (int i=0; i<sz; i++){
+                auto [r,c] = q.front();
+                q.pop();
+                for (int k=0; k<4; k++){
+                    int nr = r+dr[k];
+                    int nc = c+dc[k];
+                    if (0 <= nr && nr < m &&
+                        0 <= nc && nc < n &&
+                        grid[nr][nc] == 1){
+                            grid[nr][nc] = 2;
+                            q.push({nr,nc});
+                            fresh--;
+                            madeRotten = true;
+                        }
+                }
             }
             if (madeRotten)
                 minutes++;
         }
-        return (fresh == 0) ? minutes : -1;
+        return fresh == 0 ? minutes : -1;
     }
 };
