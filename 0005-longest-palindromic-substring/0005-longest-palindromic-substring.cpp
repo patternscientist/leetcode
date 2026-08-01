98d68fd50a282ba2e38ef3163c1efb8fc44dd3f7
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-08-01 15:32:53 -0700

    Time: 12 ms (50%), Space: 14.9 MB (38.79%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
+        int best = 0;
+        string ans;
         int n = (int)s.size();
-        int bestStart = 0, bestLen = 1;
-        auto expand = [&](int l, int r){
-            while (l >= 0 && r < n && (s[l] == s[r])){
+        auto expand = [&s,&best,&ans,n](int l, int r){
+            while (l >= 0 && r < n && s[l] == s[r]){
                 l--;
                 r++;
             }
             int len = r-l-1;
-            if (len > bestLen){
-                bestLen   = len;
-                bestStart = l+1;
-            }
+            best = max(best,len);
+            if (best == len)   
+                ans = s.substr(l+1,len);
         };
-
         for (int i=0; i<n; i++){
             expand(i,i);
             expand(i,i+1);
         }
-        return s.substr(bestStart,bestLen);
+        return ans;
     }
 };
