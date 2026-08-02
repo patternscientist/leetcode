5c0d1037e2de3e5ef1ef1815426c885dfcf67889
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-08-01 17:03:30 -0700

    Time: 3 ms (71.61%), Space: 10.6 MB (86.34%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -3,18 +3,18 @@ public:
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> wordSet(wordDict.begin(),wordDict.end());
         int maxLen = 0;
-        for (const string& w : wordDict)
-            maxLen = max(maxLen, (int)w.size());
-        int n = (int)s.size();
+        for (const string& word : wordDict)
+            maxLen = max(maxLen, (int)word.size());
+        cout << "maxLen: " << maxLen << endl;
+        int n = (int)s.size(); // length of string
         vector<bool> dp(n+1,false);
         dp[0] = true;
         for (int i=1; i<=n; i++){
             for (int len=1; len<=min(i,maxLen); len++){
                 int j = i-len;
-                if (dp[j] && wordSet.contains(s.substr(j,len))){
-                    dp[i] = true;
+                dp[i] = dp[j] && wordSet.contains(s.substr(j,len));
+                if (dp[i])
                     break;
-                }
             }
         }
         return dp[n];
