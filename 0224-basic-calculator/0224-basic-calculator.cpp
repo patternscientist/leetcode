f964e17bd098bd959ffe838ee9b177a9cb1974e6
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-08-04 21:45:51 -0700

    Time: 1 ms (70.28%), Space: 12.4 MB (93.21%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int parse(const string& s, int& i){
+        long long ans = 0;
+        int sign = 1;
+        int n = (int)s.size();
+        while (i < n){
+            if (s[i] == ' ')
+                i++;
+            else if (s[i] == '+'){
+                sign = 1;
+                i++;
+            } else if (s[i] == '-'){
+                sign = -1;
+                i++;
+            } else if (isdigit(s[i])){
+                long long num = 0;
+                while (i < n and isdigit(s[i])){
+                    num = num * 10 + (s[i]-'0');
+                    i++;
+                }
+                ans += sign * num;
+                sign = 1;
+            } else if (s[i] == '('){
+                i++;
+                int value = parse(s,i);
+                ans += sign * value;
+                sign = 1;
+            } else if (s[i] == ')'){
+                i++;
+                return ans;
+            }
+        }
+        return ans;
+    }
+    int calculate(string s) {
+        int i = 0;
+        return parse(s,i);
+    }
+};
