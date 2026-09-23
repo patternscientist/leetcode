4074e4901d3c683df7c1c2b7299f9b7a0bbdd5f4
D. D. Poindexter, Jr <noitcartsba@proton.me>
2026-09-23 15:58:26 -0700

    Time: 3 ms (75.43%), Space: 19.3 MB (80.46%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,33 +1,33 @@
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
+    def orangesRotting(self, grid: list[list[int]]) -> int:
+        m  = len(grid)
+        n  = len(grid[0])
+        dr = (0,-1,0,1)
+        dc = (-1,0,1,0)
+        q  = deque()
         fresh = 0
-        q = deque()
+        minutes = 0
         for r in range(m):
             for c in range(n):
                 if grid[r][c] == 2:
                     q.append((r,c))
                 elif grid[r][c] == 1:
                     fresh += 1
-        dr = (0,-1,0,1)
-        dc = (-1,0,1,0)
-        minutes = 0
         while len(q) != 0:
-            madeRotten = False
             sz = len(q)
+            madeRotten = False
             for _ in range(sz):
                 (r,c) = q.popleft()
                 for k in range(4):
-                    nr = r+dr[k]
-                    nc = c+dc[k]
-                    if (0 <= nr and nr < m and
-                        0 <= nc and nc < n and 
-                        grid[nr][nc] == 1):
+                    nr = r + dr[k]
+                    nc = c + dc[k]
+                    if (0 <= nr and nr < m 
+                        and 0 <= nc and nc < n 
+                        and grid[nr][nc] == 1):
                         grid[nr][nc] = 2
                         q.append((nr,nc))
                         fresh -= 1
-                        madeRotten = True
+                        madeRotten = True 
             if madeRotten:
                 minutes += 1
         return minutes if fresh == 0 else -1
